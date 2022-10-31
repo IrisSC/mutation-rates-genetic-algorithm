a5ec32048e785d18907f7c8ac6d833703fe8c762
IrisSC <shakerci@dickinson.edu>
2022-10-30 22:08:13 -0400

    Created list of tests that will need to be written.

---
+++ MutationRatesGeneticAlgorithm/src/geneticAlgorithm/PopulationTests.java
@@ -0,0 +1,14 @@
+package geneticAlgorithm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PopulationTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
