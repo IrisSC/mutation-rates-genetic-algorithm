f28ff63a110c26d1be1d270dfb44ae324bca1b26
IrisSC <shakerci@dickinson.edu>
2022-11-05 12:20:54 -0400

    Tested getBestFirIndiv. Set up sets for getParent and generate population.These last two tests still need to be completed.

--- MutationRatesGeneticAlgorithm/src/geneticAlgorithm/PopulationTests.java
+++ MutationRatesGeneticAlgorithm/src/geneticAlgorithm/PopulationTests.java
@@ -19,7 +19,7 @@ class PopulationTests {
 		
 		assertEquals("check the numPop is 50", pop1.getNumPopulation(), 50);
 		assertEquals("check the population list size is 50",
-				pop1.getPopultion().size(), 50);
+				pop1.getPopulation().size(), 50);
 	}
 	
 	@Test
@@ -41,7 +41,7 @@ class PopulationTests {
 		
 		assertEquals("check the numPop is 4", pop.getNumPopulation(), 4);
 		assertEquals("check the population list size is 4",
-				pop.getPopultion().size(), 4);
+				pop.getPopulation().size(), 4);
 	}
 	
 	@Test
@@ -148,4 +148,67 @@ class PopulationTests {
 		
 		assertEquals("stanard deviation fitness value equals 61.54", pop.stanDevFitness(), 61.54, 0);
 	}
+	
+	@Test
+	void returnBestFitIndiv() {
+		int[] solution1 = {0, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+		int[] solution2 = {0, 1, 0, 0, 1, 1, 0, 0, 0, 1};
+		int[] solution3 = {0, 1, 1, 1, 1, 0, 1, 0, 1, 0};
+		int[] solution4 = {0, 1, 0, 0, 0, 1, 0, 0, 1, 0};
+		
+		IndivStaticMutate indiv1 = new IndivStaticMutate(solution1, 0.2);
+		IndivStaticMutate indiv2 = new IndivStaticMutate(solution2, 0.2);
+		IndivStaticMutate indiv3 = new IndivStaticMutate(solution3, 0.2);
+		IndivStaticMutate indiv4 = new IndivStaticMutate(solution4, 0.2);
+		
+		List<Individual> indivList = new ArrayList<Individual>();
+		indivList.add(indiv1);
+		indivList.add(indiv2);
+		indivList.add(indiv3);
+		indivList.add(indiv4);
+		
+		KnapSack1 sack1 = new KnapSack1();
+		
+		Population pop = new Population(indivList, sack1);
+		
+		Individual bestFit = pop.getBestFitIndiv();
+		
+		assertEquals("best fit indiviudal's fitness is 159", sack1.fitness(bestFit), 159);
+		
+		assertEquals("best fit indiviudal's doesn't have item 0", bestFit.getSolutionChromosome()[0], 0);
+		assertEquals("best fit indiviudal's does have item 1", bestFit.getSolutionChromosome()[1], 1);
+		assertEquals("best fit indiviudal's does have item 4", bestFit.getSolutionChromosome()[4], 1);
+		assertEquals("best fit indiviudal's doesn't have item 5", bestFit.getSolutionChromosome()[5], 0);
+		assertEquals("best fit indiviudal's doesn't have item 9", bestFit.getSolutionChromosome()[9], 0);
+	}
+	
+	@Test
+	void getParent() {
+		int[] solution1 = {0, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+		int[] solution2 = {0, 1, 0, 0, 1, 1, 0, 0, 0, 1};
+		int[] solution3 = {0, 1, 1, 1, 1, 0, 1, 0, 1, 0};
+		int[] solution4 = {0, 1, 0, 0, 0, 1, 0, 0, 1, 0};
+		
+		IndivStaticMutate indiv1 = new IndivStaticMutate(solution1, 0.2);
+		IndivStaticMutate indiv2 = new IndivStaticMutate(solution2, 0.2);
+		IndivStaticMutate indiv3 = new IndivStaticMutate(solution3, 0.2);
+		IndivStaticMutate indiv4 = new IndivStaticMutate(solution4, 0.2);
+		
+		List<Individual> indivList = new ArrayList<Individual>();
+		indivList.add(indiv1);
+		indivList.add(indiv2);
+		indivList.add(indiv3);
+		indivList.add(indiv4);
+		
+		KnapSack1 sack1 = new KnapSack1();
+		
+		Population pop = new Population(indivList, sack1);
+		//create a Individual equals method in the indvidual classes
+	}
+	
+	@Test
+	void newPopulation() {
+		//not sure how to test
+	}
+	
 }
