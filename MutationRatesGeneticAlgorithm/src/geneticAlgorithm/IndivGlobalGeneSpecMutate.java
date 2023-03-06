3e8b8cf2ec5a86b2bddfe556cc9554434b412465
IrisSC <shakerci@dickinson.edu>
2023-03-06 11:01:13 -0500

    Added Global Gene Specific mutation rate individual

---
+++ MutationRatesGeneticAlgorithm/src/geneticAlgorithm/IndivGlobalGeneSpecMutate.java
@@ -0,0 +1,54 @@
+package geneticAlgorithm;
+
+import java.util.List;
+
+public class IndivGlobalGeneSpecMutate extends Individual{
+
+	public IndivGlobalGeneSpecMutate(int solutionLength) {
+		super(solutionLength);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean equals(Individual indiv) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void mutation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Individual> crossover(Individual in) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Individual createRandomIndiv() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Individual copy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getMutationRate() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
