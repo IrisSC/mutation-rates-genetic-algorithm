7d4a2a817d82a4f852b00632d7ff29cda6628a3c
IrisSC <shakerci@dickinson.edu>
2023-04-02 18:00:54 -0400

    Added Kanpsack 11 which has 300 items in it.

--- MutationRatesGeneticAlgorithm/src/geneticAlgorithm/GeneticAlgorithm.java
+++ MutationRatesGeneticAlgorithm/src/geneticAlgorithm/GeneticAlgorithm.java
@@ -45,7 +45,8 @@ public class GeneticAlgorithm {
 		//Knapsack4 sack1 = new Knapsack4();
 		//Knapsack5 sack1 = new Knapsack5();
 		//Knapsack9 sack1 = new Knapsack9();
-		Knapsack10 sack1 = new Knapsack10();
+		//Knapsack10 sack1 = new Knapsack10();
+		Knapsack11 sack1 = new Knapsack11();
 		//Knapsack6 sack1 = new Knapsack6();
 		//Knapsack7 sack2 = new Knapsack7();
 		//Knapsack8 sack3 = new Knapsack8();
